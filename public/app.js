79870697cd5774abd5156dd826eeb2214d42b14a
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-02-09 03:06:06 -0800

    added images and cards with card reveal effect to html

--- public/app.js
+++ public/app.js
@@ -2,15 +2,31 @@
 $("#accept").click(e => {
     e.preventDefault();
     $("#invitation").css("display", "none");
+    renderWorkouts();
+})
+
+// Decline button 
+$("#decline").click(e => {
+    e.preventDefault();
+    $("#invitation").css("display", "none");
+    $("#wimp").css("display", "block");
+})
+
+// Okay button returns to invitation view
+$("#okay").click(e => {
+    e.preventDefault();
+    $("#wimp").css("display", "none");
+    $("#invitation").css("display", "block")
+
+})
+
+// Render a week of workouts
+function renderWorkouts() {
+    $("#days").empty();
     $.ajax({
         url: "/populateddays",
         method: "GET",
     }).then(data => {
-        console.log(`Here's your workouts, friend: ${JSON.stringify(data, null, 2)}`)
+        console.log(`Here's your data: ${JSON.stringify(data, null, 2)}`)
     })
-})
-
-$("#decline").click(e => {
-    e.preventDefault();
-    $("invitation").css("display", "none")
-})
+}
